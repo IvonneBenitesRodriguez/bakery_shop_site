8ca4a392045e8cab5ad3436ab0a44a8ef61e2477
Ivonne <ivonnebenites2015@gmail.com>
2024-07-28 23:17:32 +0200

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from "./components/Header.jsx";
 import Carousel from "./components/Carousel.jsx";
 import BannerOne from "./components/BannerOne.jsx";
 import PopularCategories from "./components/PopularCategories.jsx";
+import SameDay from "./components/SameDay.jsx";
+import SameDayCarouselData from "./components/SameDayCarouselData.jsx";
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
       <Carousel />
       <BannerOne />
       <PopularCategories />
+      <SameDay />
+      <SameDayCarouselData />
     </>
   );
 };
