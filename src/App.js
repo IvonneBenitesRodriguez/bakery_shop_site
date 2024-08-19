d518856ffaeb15034967eeee33c1dd8f4853916b
Chandan-devs-tech <chandanguptabsb@gmail.com>
2024-08-20 03:09:17 +0530

    Modify src/App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/homepage/Header';
 import Carousel from './components/homepage/Carousel';
 import BannerOne from './components/homepage/BannerOne';
@@ -8,19 +9,30 @@ import ProductCarousel from './components/homepage/JustBecause';
 import BannerCookies from './components/homepage/BannerCookies';
 import CookieCarousel from './components/homepage/CookieCarousel';
 import FloatingLabelInput from './components/homepage/Discount_Banner';
+import Footer from './components/homepage/Footer';
 
 const App = () => (
-  <>
+  <Router>
     <Header />
-    <Carousel />
-    <BannerOne />
-    <PopularCategories />
-    <SameDayCarouselData />
-    <ProductCarousel />
-    <BannerCookies />
-    <CookieCarousel />
-    <FloatingLabelInput />
-  </>
+    <Routes>
+      <Route
+        path="/"
+        element={(
+          <>
+            <Carousel />
+            <BannerOne />
+            <PopularCategories />
+            <SameDayCarouselData />
+            <ProductCarousel />
+            <BannerCookies />
+            <CookieCarousel />
+            <FloatingLabelInput />
+          </>
+        )}
+      />
+    </Routes>
+    <Footer />
+  </Router>
 );
 
 export default App;
